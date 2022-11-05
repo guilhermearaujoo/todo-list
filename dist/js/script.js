c8721e44d321fc424ab8a661afd807645e36bda3
guilhermearaujoo <ghma.em1@gmail.com>
2022-11-04 22:30:03 -0300

    used hof to make it the background function easier

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,29 @@
 // add a task on the page whanever the user submits
 const btnAddTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-
 // changes the backgroundcolor of an item when clicked
-const changeBackground = (li) => {
-  li.addEventListener('click', () => {
-    li.classList.contains('clicked') ? li.classList.remove('clicked') : li.classList.add('clicked');
-  });
+const changeBackground = (lis) => {
+  lis.forEach((element) => {
+    element.addEventListener('click', () => {
+      lis.forEach((li) => li.classList.remove('clicked'));
+      element.classList.add('clicked');
+    });
+  })
 }
 
 const submitItem = () => {
   const inputValue = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
   li.innerText = inputValue;
+  li.className = 'list-item';
   taskList.appendChild(li);
-  changeBackground(li);
 }
 
 window.addEventListener('click', (event) => {
   if(event.target.id === 'criar-tarefa') {
     submitItem();
+    const lis = document.querySelectorAll('.list-item');
+    changeBackground(lis);
   }
 })
 
